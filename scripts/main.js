66a068724b25a6a4e20fe43f1d4d2af9694ae595
Chris Rithmyxay <crithmyxay@gmail.com>
2018-03-16 01:13:05 -0400

    fixed jQuery and hamburger icon to work

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,20 @@
 // var controller = new ScrollMagic.Controller();
 // var scene = new ScrollMagic.Scene();
 
-window.addEventListener('load', AOS.refresh)
+// window.addEventListener('load', AOS.refresh)
 
-$("#myNavBar").find("a").click(function(e) {
-  e.preventDefault();
-  var section = $(this).attr("href");
-  $("html, body").animate({
-      scrollTop: $(section).offset().top
+// $("#myNavBar").find("a").click(function(e) {
+//   e.preventDefault();
+//   var section = $(this).attr("href");
+//   $("html, body").animate({
+//       scrollTop: $(section).offset().top
+//   });
+// });
+
+$(document).ready(function() {
+  $('button').click( function (event) {
+    $('.dropdown').toggleClass('hidden');
+    console.log('clicked');
+    // $('.dropdown').toggleClass('hidden');
   });
-});
-
-$("[data-collapse]").click(() => {
-  console.log('clicked');
-  $(".dropdown").toggleClass("hidden");
 });
